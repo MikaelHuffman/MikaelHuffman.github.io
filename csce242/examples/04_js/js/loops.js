5a95ca089aa7d67c6dc7e12ff0515f3f596dc14c
MikaelHuffman <huffmanmikael@gmail.com>
2025-09-25 12:42:10 -0400

    in class work

--- csce242/examples/04_js/js/loops.js
+++ csce242/examples/04_js/js/loops.js
@@ -10,7 +10,7 @@ document.getElementById("btn-display").onclick = () => {
         li.innerHTML = `${i} banana(s)`;
 
     }
-}
+};
 
 document.getElementById("btn-count").onclick = () => {
     const error = document.getElementById("error-number");
@@ -26,4 +26,63 @@ document.getElementById("btn-count").onclick = () => {
         error.innerHTML = "* The second number must be greater than the first";
         return;
     }
-}
+
+    // Loop for every number from start to end display the number in paragraphs
+    // if they did 5 and 9, it should go 5, 6, 7, 8, 9
+
+    for(let i = startNum; i<=endNum; i++) {
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        resultDiv.append(p);
+        p.onclick = () => {
+            console.log(`You clicked Number ${i}`);
+        };
+    }
+};
+
+document.getElementById("btn-show-toys").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = "";
+
+    const toys = ["ball", "skipping rope", "doll", "mini car", "elmo"];
+
+    // First way to iterate (bad)
+    /*for(let i = 0; i <= toys.length; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = toys[i];
+        toyList.append(li);
+    }*/
+
+    // preferred way to iterate
+    toys.forEach((toy, i)=>{
+        const li = document.createElement("li");
+        li.innerHTML = toy;
+        toyList.append(li);
+    });
+};
+
+document.getElementById("btn-show-toy-prices").onclick = () => {
+    const toys = [];
+    toys["ball"] = 2.99;
+    toys["barbie"] = 19.99;
+    toys["skipping rope"] = 1.98;
+    toys["match box car"] = 2.54;
+
+    const toyDiv = document.getElementById("toy-prices");
+    toyDiv.innerHTML = "";
+
+    const priceP = document.getElementById("price-p");
+    priceP.innerHTML = "";
+
+    for(let toy in toys){
+        const p = document.createElement("p");
+        p.innerHTML = toy;
+        toyDiv.append(p);
+
+        //when i click paragraph, show price in a div below
+
+        p.onclick = () => {
+            priceP.innerHTML = `${toy} costs ${toys[toy]}`;
+        };
+    }
+};
